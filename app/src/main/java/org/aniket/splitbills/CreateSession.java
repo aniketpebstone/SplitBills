3d88ffaf9c9bd331301ff2ef2a10a1cfde2937e5
aniket <aniket.paranjpe@pebstone.com>
2019-07-10 20:27:53 +0530

    main screen and create session screen created

---
+++ app/src/main/java/org/aniket/splitbills/CreateSession.java
@@ -0,0 +1,14 @@
+package org.aniket.splitbills;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_session);
+    }
+}
--- app/src/main/java/org/aniket/splitbills/MainActivity.java
+++ app/src/main/java/org/aniket/splitbills/MainActivity.java
@@ -2,7 +2,11 @@ package org.aniket.splitbills;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToCreateSessionScreen(View view) {
+
+        Intent intent = new Intent(this, CreateSession.class);
+        startActivity(intent);
+
+    }
+
+    public void goToLoadSessionScreen(View view) {
+
+        Intent intent = new Intent(this, CreateSession.class);
+        startActivity(intent);
+
+    }
+    public void goToDeleteSessionScreen(View view) {
+
+        Intent intent = new Intent(this, CreateSession.class);
+        startActivity(intent);
+
     }
 }
