b2c173b63e34a8d68b43279d0f4ec3aa85f6259c
aniket <aniket.paranjpe@pebstone.com>
2019-07-11 12:45:45 +0530

    some string constants added and back button implemented

--- app/src/main/java/org/aniket/splitbills/CreateSession.java
+++ app/src/main/java/org/aniket/splitbills/CreateSession.java
@@ -2,7 +2,9 @@ package org.aniket.splitbills;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateSession extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class CreateSession extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_session);
+    }
+
+    public void createNewSession(View view) {
+
+
+    }
+
+    public void cancelCreateSession(View view) {
+        finish();
     }
 }
